7d97394699e4e6cd3c22cd6926a4f0b41bc4ab1f
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2022-09-05 16:30:11 +0430

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+import hashlib
+
+
+def calc_sha256(str):
+    return hashlib.sha256(str.encode())
+
+
+class CoinTable:
+    def __init__(self, id, abou, stat, type, n_in_co_ring, p_in_co_ring, b_on, owner):
+        self.coin_id = id
+        self.amount_based_on_one_unit = abou
+        self.status = stat  # run - blocked - expired/paid
+        self.type = type
+        self.next_in_cooporation_ring = n_in_co_ring
+        self.previous_in_cooperation_link = p_in_co_ring
+        self.binded_on = b_on
+        self.owner = owner
+
+
+class CoOperationTable:
+    def __init__(self, id, ngm, w, nifr, pifr, p_in_co_ring, owner, nrr):
+        self.group_id = id
+        self.number_of_group_members = ngm
+        self.weight = w  # run - blocked - expired/paid
+        self.next_in_fractal_ring = nifr
+        self.previous_in_fractal_ring = pifr
+        self.owner = owner
+        self.number_of_required_rounds = nrr
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print(calc_sha256("GeeksforGeeks").hexdigest())
+    coin = CoinTable(1, 1, 'ready', 'x', None, None, None, 'someone')
+    print(coin.owner)
+
+    from web3 import Web3
+
+    infura_url = 'https://mainnet.infura.io/v3/81d6d934a6584310978f5d82c58bc1c6'
+    print(infura_url)
+
+    # HTTPProvider:
+    w3 = Web3(Web3.HTTPProvider(infura_url))
+    res = w3.isConnected()
+    print(res)
+    print(w3.eth.blockNumber)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
