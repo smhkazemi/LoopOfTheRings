e2d06c1254809416ce55db21e15abca30462a83a
Seyed Mohammad Hussein Kazemi <smhk1375@icloud.com>
2022-09-19 16:12:31 +0430

    Adding all algorithms of the paper + defining the rate of epsilon as the rate used to determine the trusted users.

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ from web3 import Web3
 import time
 import random
 
-users = []
+epsilon = 0.001
+
+users = {}
 
 id_to_sha_table = {}
 id_to_list_of_invested_coins = {}
@@ -23,17 +25,26 @@ coin_list_typing = []
 
 fractal_ring_dict = {}
 
+ids_in_system = {}
 
 def get_random_id():
     random.seed(int(round(time.time() * 1000)))
-    return int(random.random() * 100000000)
+    num = int(random.random() * 1000000000)
+    while num in ids_in_system.keys():
+        num = int(random.random() * 1000000000)
+    return num
 
 
 class Trader:
-    def __init__(self, ara, usd):
+    def __init__(self, ara):
         self.id = get_random_id()
         self.ara_amount = ara
-        self.credit_usd = usd
+
+
+def register_a_user(ara):
+    user = Trader(ara)
+    users[user.id] = user
+    return user.id
 
 
 def calc_sha256(str):
@@ -41,12 +52,14 @@ def calc_sha256(str):
 
 
 def register_a_coin(owner, abou, type, nrr):
+    if owner.ara_amount < abou:
+        raise ("Not enough Ara for trader: " + owner.id)
     coin_table = CoinTable(get_random_id(), abou, 'run', type, None, None, None, owner)
     if 'I' in type:
         if owner.id not in coin_dict_invest:
             coin_dict_invest[owner.id] = []
         coin_dict_invest[owner.id].append(coin_table)
-        if (create_co_op_ring(coin_table, 10)) != None:
+        if (create_co_op_ring(coin_table, 10)) is not None:
             if owner.id not in id_to_list_of_invested_coins.keys():
                 id_to_list_of_invested_coins[owner.id] = []
             id_to_list_of_invested_coins[owner.id].append(coin_dict_invest[-1])
@@ -104,6 +117,24 @@ def randomized_number_of_cooperation_ring():
 
 
 def submit_a_fractal_ring(owner, average_number_of_fractal_rings, rand_num_of_co_rings):
+    id = generate_fractal_ring(average_number_of_fractal_rings, owner, rand_num_of_co_rings)
+    if id is None:
+        print("Failed to generate the fractal ring for the owner: " + owner.id)
+        return None
+    fractal_ring_dict[id].append(select_members_of_the_verification_team_for(id))
+    return id
+
+
+def select_members_of_the_verification_team_for(id):
+    vt = (int(hashlib.sha256(str(h).encode()).hexdigest(), 32) % len(fractal_ring_dict[id][0])) + 503
+    result = []
+    list_of_users = users.values()
+    for i in range(0, vt):
+        result.append(list_of_users[get_random_index_sha256(len(list_of_users))])
+    return result
+
+
+def generate_fractal_ring(average_number_of_fractal_rings, owner, rand_num_of_co_rings):
     if owner.id not in co_operation_table_dict.keys():
         return None
     if len(co_operation_table_dict[owner.id]) < rand_num_of_co_rings:
@@ -125,7 +156,7 @@ def submit_a_fractal_ring(owner, average_number_of_fractal_rings, rand_num_of_co
             result[i - 1].previous_in_fractal_ring = result[i - 2]
         else:
             result[i - 1].previous_in_fractal_ring = None
-    fractal_ring_dict[owner.id] = result
+    fractal_ring_dict[owner.id] = [result, get_random_id()]
     return owner.id
 
 
@@ -155,6 +186,12 @@ class CoOperationTable:
         self.previous_in_fractal_ring = pifr
         self.owner = owner
         self.number_of_required_rounds = nrr
+
+
+def coin_registration_unit_test():
+    user_id_1 = register_a_user(10)
+    print("registered ")
+    user_id_2 = register_a_user(20)
 
 
 def ether_connection_unit_test():
