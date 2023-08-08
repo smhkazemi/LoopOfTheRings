3961b91e7a317d3df28ed8a422ae14cea9e277a6
Seyed Mohammad Hussein Kazemi <32571099+smhkazemi@users.noreply.github.com>
2023-08-08 22:11:36 +0330

    Update initiator.sol

--- initiator.sol
+++ initiator.sol
@@ -2,11 +2,10 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 import "./trader.sol";
-import "./broadcast_sim.sol";
 
 interface broadcast_interface{
     function switch_to_lor() external;
-    function add_new_user_by_address(address adr, uint256 id) external;
+    function add_new_user_by_address(address adr, uint256 id, address v_address) external;
 }
 
 contract initiator{
@@ -16,10 +15,10 @@ contract initiator{
     uint256 private num_of_traders_signed_up;
     address private broadcast_addr;
 
-    constructor() public {
+    constructor(address b_address) {
         switched_to_lor = false;
         num_of_traders_signed_up = 0;
-        broadcast_addr = address(new broadcast_sim());
+        broadcast_addr = b_address;
     }
 
     function rand_id() private returns(uint256)
@@ -30,18 +29,18 @@ contract initiator{
     }
 
     // The admin desires to signup a new trader
-    function sign_up(int ara) external returns(address){
+    function sign_up(int ara, address v_address) external returns(address){
         if(switched_to_lor == true){
             return address(0x0);
         }
         uint256 trader_id = rand_id();
-        Trader trader = new Trader(trader_id, ara, broadcast_addr);
+        Trader trader = new Trader(trader_id, ara, broadcast_addr, v_address);
         num_of_traders_signed_up++;
         if(num_of_traders_signed_up >= 1000000){
             switched_to_lor = true;
             broadcast_interface(broadcast_addr).switch_to_lor();
         }
-        broadcast_interface(broadcast_addr).add_new_user_by_address(address(trader), trader_id);
+        broadcast_interface(broadcast_addr).add_new_user_by_address(address(trader), trader_id, v_address);
         traders[trader_id] = address(trader);
         return traders[trader_id];
     }
