796f14c038ac7c9f79c2cdb4830b1dda91d57aad
Seyed Mohammad Hussein Kazemi <32571099+smhkazemi@users.noreply.github.com>
2023-08-08 22:10:45 +0330

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,60 @@ import json
 from web3.middleware import geth_poa_middleware
 
 
-def deploy(filename):
-    print("Processing the initiator solidity file...")
+def deploy(initiator_file_name, broadcast_file_name):
+    print("Please enter thee url of the http service provide (Web3.HTTPProvider):")
+    url = input("-->")  # "https://avalanche-fuji-c-chain.publicnode.com"
+    w3 = Web3(Web3.HTTPProvider(url))
+    w3.middleware_onion.inject(geth_poa_middleware, '', 0)
+    print("Please enter the chain ID:")
+    chain_id = int(input("-->"))  # 43113
+    print("Please enter the address of your account (you may find it in your MetaMask account)")
+    address = input("-->")
+    # e6b2290a4b444f3d91945d02f9c7b267
+    # 0xe72CfeDE68AF32d6354143DC35e61987BD384EEf
+
+    # 0xCE6dD32848276F9C05134A3B9aD887EaFcBBb5bB
+    # c3e16d258afdcbf16e4af44e5353ff788c5dd58d67e51b65049631c0a1a697af
+
+    if w3.is_address(address) is False:
+        print("Invalid address. Try again:")
+        address = input("-->")
+        if w3.is_address(address) is False:
+            print("Invalid address. First, make sure of the correctness of the address and the url.")
+            return
+    check_sum = w3.to_checksum_address(address)
+    if w3.eth.get_balance(check_sum) <= 0:
+        print("Not enough balance!")
+        return
+
+    print("Please enter your 32-byte private key:")
+    private_key = input("-->").encode()  # b'e6b2290a4b444f3d91945d02f9c7b267'
+    # initialize contract
+    nonce = w3.eth.get_transaction_count(address)
+    print("Processing the initiator and broadcast solidity file...")
+    initiator_abi, initiator_bytecode = get_byte_code_and_abi_from(initiator_file_name, "initiator")
+    broadcast_abi, broadcast_bytecode = get_byte_code_and_abi_from(broadcast_file_name, "broadcast_sim")
+    print("Deploying Contract with nonce: " + str(nonce))
+    transaction_dict = {
+        "chainId": chain_id,
+        "gasPrice": w3.eth.gas_price,
+        "from": address,
+        "nonce": nonce
+    }
+    broadcast_transaction_receipt = perform_transaction(private_key, transaction_dict, w3,
+                                                        w3.eth.contract(abi=broadcast_abi,
+                                                                        bytecode=broadcast_bytecode).constructor)
+    initiator_transaction_receipt = perform_transaction(private_key, transaction_dict, w3,
+                                                        w3.eth.contract(abi=initiator_abi,
+                                                                        bytecode=initiator_bytecode).constructor,
+                                                        broadcast_transaction_receipt.contractAddress)
+    # passing broadcast address
+
+    return (transaction_dict, w3.eth.contract(address=initiator_transaction_receipt.contractAddress, abi=initiator_abi),
+            w3, private_key, address)
+
+
+def get_byte_code_and_abi_from(filename, field_name):
     with open(filename, "r") as file:
         simple_storage_file = file.read()
     compiled_sol = compile_standard(
@@ -29,54 +81,14 @@ def deploy(filename):
             },
         }
     )
+    # "initiator"
     with open("compiled_code.json", "w") as file:
         json.dump(compiled_sol, file)
     print("The code was compiled to json format successfully, and the result is stored in file compiled_code.json")
     # get bytecode
-    bytecode = compiled_sol["contracts"][filename]["initiator"]["evm"]["bytecode"]["object"]
-    abi = json.loads(compiled_sol["contracts"][filename]["initiator"]["metadata"])["output"]["abi"]
-    print("Please enter thee url of the http service provide (Web3.HTTPProvider):")
-    url = input("-->")  # "https://avalanche-fuji-c-chain.publicnode.com"
-    w3 = Web3(Web3.HTTPProvider(url))
-    w3.middleware_onion.inject(geth_poa_middleware, '', 0)
-    print("Please enter the chain ID:")
-    chain_id = int(input("-->"))  # 43113
-    print("Please enter the address of your account (you may find it in your MetaMask account)")
-    address = input("-->")  # "0xCE6dD32848276F9C05134A3B9aD887EaFcBBb5bB"
-    # "Oxe72CfeDE68AF32d6354143DC35e61987BD384EEf"
-
-    # 0xe72CfeDE68AF32d6354143DC35e61987BD384EEf
-
-    # if w3.is_address(address) is False:
-    #     print("Invalid address. Try again:")
-    #     address = input("-->")
-    #     if w3.is_address(address) is False:
-    #         print("Invalid address. First, make sure of the correctness of the address and the url.")
-    #         return
-    check_sum = w3.to_checksum_address(address)
-    if w3.eth.get_balance(check_sum) <= 0:
-        print("Not enough balance!")
-        return
-
-    # "Oxe72CfeDE68AF32d6354143DC35e61987BD384EEf"
-    # "OxCE6dD32848276F9C05134A3B9aD887EaFcBBb5bB"
-    print("Please enter your 32-byte private key:")
-    private_key = input("-->").encode()  # b'e6b2290a4b444f3d91945d02f9c7b267'
-    # b'c3e16d258afdcbf16e4af44e5353ff788c5dd58d67e51b65049631c0a1a697af'
-    # 'e6b2290846444 3091945d029c762671
-    # initialize contract
-    nonce = w3.eth.get_transaction_count(address)
-    print("Deploying Contract with nonce: " + str(nonce))
-    contract_with_user_info = w3.eth.contract(abi=abi, bytecode=bytecode)
-    transaction_dict = {
-        "chainId": chain_id,
-        "gasPrice": w3.eth.gas_price,
-        "from": address,
-        "nonce": nonce
-    }
-    transaction_receipt = perform_transaction(private_key, transaction_dict, w3, contract_with_user_info.constructor)
-    initiator_contract = w3.eth.contract(address=transaction_receipt.contractAddress, abi=abi)
-    return transaction_dict, initiator_contract, w3, private_key, address
+    bytecode = compiled_sol["contracts"][filename][field_name]["evm"]["bytecode"]["object"]
+    abi = json.loads(compiled_sol["contracts"][filename][field_name]["metadata"])["output"]["abi"]
+    return abi, bytecode
 
 
 def perform_transaction(private_key, transaction_dict, w3, to_invoke, argument=None, argument2=None):
@@ -159,7 +171,8 @@ def form_co_operation_ring(investors, address, transaction_dict, private_key, w3
 
 if __name__ == '__main__':
     print("----Deploying the initiator-----")
-    transaction_dict, initiator_contract, w3, private_key, address = deploy("initiator.sol")
+    transaction_dict, initiator_contract, w3, private_key, address = deploy("initiator.sol",
+                                                                            "broadcast_sim.sol")
     print("-----Press any key to proceed-----")
     input("-->")
     number_of_users = 0
