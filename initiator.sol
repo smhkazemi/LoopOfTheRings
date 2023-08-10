62899414ac21164cc7e6387ca68716351d26bf84
Seyed Mohammad Hussein Kazemi <32571099+smhkazemi@users.noreply.github.com>
2023-08-10 05:02:30 +0330

    Update initiator.sol

--- initiator.sol
+++ initiator.sol
@@ -9,14 +9,11 @@ interface broadcast_interface{
 }
 
 contract initiator{
-    mapping (uint256 => address) private traders;
     uint private randNonce = 0;
-    bool private switched_to_lor;
     uint256 private num_of_traders_signed_up;
     address private broadcast_addr;
 
     constructor(address b_address) {
-        switched_to_lor = false;
         num_of_traders_signed_up = 0;
         broadcast_addr = b_address;
     }
@@ -30,19 +27,17 @@ contract initiator{
 
     // The admin desires to signup a new trader
     function sign_up(int ara, address v_address) external returns(address){
-        if(switched_to_lor == true){
+        if(num_of_traders_signed_up >= 1000000){
             return address(0x0);
         }
         uint256 trader_id = rand_id();
         Trader trader = new Trader(trader_id, ara, broadcast_addr, v_address);
         num_of_traders_signed_up++;
         if(num_of_traders_signed_up >= 1000000){
-            switched_to_lor = true;
             broadcast_interface(broadcast_addr).switch_to_lor();
         }
         broadcast_interface(broadcast_addr).add_new_user_by_address(address(trader), trader_id, v_address);
-        traders[trader_id] = address(trader);
-        return traders[trader_id];
+        return address(trader);
     }
 
     function get_broadcast_address() view external returns (address){
